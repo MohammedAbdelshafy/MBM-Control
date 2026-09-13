2c193e1f5996e9388f3d34c869754f649724a7af
MohammedAbdelshafy <abdelshafyclapps@gmail.com>
2026-09-13 15:55:19 +0300

    test: correct underwriting spread expectation

--- MBM/LeadEngine/real_estate/tests/test_offer_flow.py
+++ MBM/LeadEngine/real_estate/tests/test_offer_flow.py
@@ -38,7 +38,7 @@ UNDERWRITING = underwrite(arv=200000, repairs=30000, purchase_price=70000)
 
 def test_underwrite_calculates_mao_and_economic_gate():
     assert UNDERWRITING["mao"] == 110000
-    assert UNDERWRITING["gross_spread"] == 130000
+    assert UNDERWRITING["gross_spread"] == 100000
     assert UNDERWRITING["passes_economic_gate"] is True
     assert qualifies_for_human_review(UNDERWRITING, "ok", True) is True
 
