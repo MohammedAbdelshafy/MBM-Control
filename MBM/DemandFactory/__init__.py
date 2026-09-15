21cad13d88a505f5ca834c243135ac674806cd4d
MohammedAbdelshafy <abdelshafyclapps@gmail.com>
2026-09-15 23:06:00 +0300

    feat(factory): expose conviction primitives

--- MBM/DemandFactory/__init__.py
+++ MBM/DemandFactory/__init__.py
@@ -1,12 +1,24 @@
 from .engine import DemandFactory, FactoryResult, utc_now
-from .models import Decision, DemandSignal, OfferCandidate, Opportunity
+from .models import (
+    ConvictionAssessment,
+    ConvictionGateResult,
+    Decision,
+    DemandSignal,
+    OfferCandidate,
+    Opportunity,
+)
+from .quality import ProductQualityContract, validate_quality_contract
 
 __all__ = [
+    "ConvictionAssessment",
+    "ConvictionGateResult",
     "Decision",
     "DemandFactory",
     "DemandSignal",
     "FactoryResult",
     "OfferCandidate",
     "Opportunity",
+    "ProductQualityContract",
+    "validate_quality_contract",
     "utc_now",
 ]
