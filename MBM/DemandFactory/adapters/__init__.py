ddab5948d6fca2d23c54e5389136f1e403716ea1
MohammedAbdelshafy <abdelshafyclapps@gmail.com>
2026-09-15 23:05:34 +0300

    feat(factory): add adapter package

---
+++ MBM/DemandFactory/adapters/__init__.py
@@ -0,0 +1,10 @@
+from .higgsfield import HiggsfieldCreativeAdapter
+from .hubspot import HubSpotCommercialAdapter
+from .knowledge_graph import KnowledgeGraphAdapter, NullKnowledgeGraphAdapter
+
+__all__ = [
+    "HiggsfieldCreativeAdapter",
+    "HubSpotCommercialAdapter",
+    "KnowledgeGraphAdapter",
+    "NullKnowledgeGraphAdapter",
+]
